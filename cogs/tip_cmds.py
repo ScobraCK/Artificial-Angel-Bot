0a4b0b1866813f47457c3ecc479ff476263a95c2
ScobraCk <scobrack@gmail.com>
2023-04-11 17:35:58 +0900

    added dailyinfo

--- cogs/tip_cmds.py
+++ cogs/tip_cmds.py
@@ -3,6 +3,70 @@ from discord import app_commands
 from discord.ext import commands
 
 import common
+import timezones
+from typing import Optional
+from io import StringIO
+from my_view import My_View
+
+def get_dailyinfo(server: timezones.ServerUTC):
+    text = StringIO()
+    info = timezones.DailyEvents()
+    local = timezones.unix_to_local
+    text.write(f'<t:{local(info.reset, server)}:t> Daily Server Reset\n')
+    text.write(f'<t:{local(info.shop1, server)}:t> Free Shop Reset A\n')
+    text.write(f'<t:{local(info.shop2, server)}:t> Free Shop Reset B\n')
+    text.write(f'<t:{local(info.shop3, server)}:t> Free Shop Reset C\n')
+    text.write(f'<t:{local(info.shop4, server)}:t> Free Shop Reset D\n\n')
+    text.write(f'<t:{local(info.temple_open1, server)}:t>~<t:{local(info.temple_close1, server)}:t> Temple of Illusion A\n')
+    text.write(f'<t:{local(info.temple_open2, server)}:t>~<t:{local(info.temple_close2, server)}:t> Temple of Illusion B\n\n')
+    text.write(f'<t:{local(info.guild_strategy_start, server)}:t>~<t:{local(info.guild_strategy_end, server)}:t> Guild Battle: Strategy Phase\n')
+    text.write(f'<t:{local(info.guild_war_start, server)}:t>~<t:{local(info.guild_war_end, server)}:t> Guild Battle: War Phase\n\n')
+    text.write(f'<t:{local(info.grand_strategy_start, server)}:t>~<t:{local(info.grand_strategy_end, server)}:t> Grand Battle: Strategy Phase\n')
+    text.write(f'<t:{local(info.grand_war_start, server)}:t>~<t:{local(info.grand_war_end, server)}:t> Grand Battle: War Phase\n\n')
+
+    return text.getvalue()
+
+def daily_embed(server: timezones.ServerUTC):
+    server_name = server.name.replace('_', '/')
+
+    embed=discord.Embed(
+        title = f'Daily Information [{server_name}]',
+        description=get_dailyinfo(server),
+        color=discord.Colour.blue()
+        )
+    return embed
+
+class DailyView(My_View):
+    def __init__(self, user: discord.User):
+        super().__init__(user)
+
+    async def update_button(self, button: discord.ui.Button):
+        for btn in self.children:
+            if btn is button:
+                btn.disabled=True
+            else:
+                btn.disabled=False
+
+    @discord.ui.button(label="NA")
+    async def na_btn(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await self.update_button(button)
+        await interaction.response.edit_message(embed=daily_embed(timezones.ServerUTC.NA), view=self)
+        
+    @discord.ui.button(label="EU/GL")
+    async def eu_btn(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await self.update_button(button)
+        await interaction.response.edit_message(embed=daily_embed(timezones.ServerUTC.EU_GL), view=self)
+
+    @discord.ui.button(label="JP/KR")
+    async def jp_btn(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await self.update_button(button)
+        await interaction.response.edit_message(embed=daily_embed(timezones.ServerUTC.JP_KR), view=self)
+
+    @discord.ui.button(label="ASIA")
+    async def asia_btn(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await self.update_button(button)
+        await interaction.response.edit_message(embed=daily_embed(timezones.ServerUTC.ASIA), view=self)
+
 
 class Tips(commands.Cog, name='Other Commands'):
     '''These are helpful tip commands'''
@@ -50,6 +114,31 @@ class Tips(commands.Cog, name='Other Commands'):
         await interaction.response.send_message(
             embed=embed
         )
+
+    @app_commands.command()
+    async def dailyinfo(self, interaction: discord.Interaction, 
+                        server: Optional[timezones.ServerUTC] = timezones.ServerUTC.NA):
+        '''List of notable daily events shown in local time'''
+        
+        embed = daily_embed(server)
+
+        user = interaction.user
+        view = DailyView(user)
+
+        if server == timezones.ServerUTC.NA:
+            first_btn = view.na_btn
+        elif server == timezones.ServerUTC.JP_KR:
+            first_btn = view.jp_btn
+        elif server == timezones.ServerUTC.EU_GL:
+            first_btn = view.eu_btn
+        else:
+            first_btn = view.asia_btn
+
+        await view.update_button(first_btn)
+        await interaction.response.send_message(embed=embed, view=view)
+
+        message = await interaction.original_response()
+        view.message = message
 
 
 async def setup(bot):
---
+++ timezones.py
@@ -0,0 +1,60 @@
+from enum import Enum
+from datetime import datetime, timezone
+
+class ServerUTC(Enum):
+    NA = -7
+    JP_KR = 9
+    EU_GL = 1
+    ASIA = 8
+
+
+class DailyEvents():
+    reset = 1680840000
+    shop1 = 1680771600
+    shop2 = 1680782400
+    shop3 = 1680793200
+    shop4 = 1680804000
+    battle_league = 1680813000
+    temple_open1 = 1680784200
+    temple_close1 = 1680787800
+    temple_open2 = 1680809400
+    temple_close2 = 1680813000
+    guild_strategy_start = 1680853500  # guild war
+    guild_strategy_end = 1680813000
+    guild_war_start = 1680813900
+    guild_war_end = 1680816600
+    #grand_first_strategy = 1680808500
+    grand_strategy_start = 1680853500
+    grand_strategy_end = 1680813900
+    grand_war_start = 1680817500
+    grand_war_end = 1680820200
+
+
+def convert_date_string(date: datetime):
+    return datetime.strftime(date, '%Y-%m-%d %H:%M:%S')
+
+def convert_date(date: str)->datetime:
+    return datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+
+def convert_from_local(date: str, utc_diff: int)->int:
+    '''returns unix timestamp from server time string'''
+    return int(convert_date(date).replace(tzinfo=timezone.utc).timestamp()) - utc_diff*3600
+
+def convert_from_jst(date: str)->int:
+    '''returns unix timestamp from jst time string'''
+    return int(convert_date(date).replace(tzinfo=timezone.utc).timestamp()) - 32400 # -9 hours
+
+def local_to_unix(timestamp: datetime.timestamp, server: ServerUTC):
+    '''converts local timestamps to unix'''
+    return timestamp + server.value*3600
+
+def unix_to_local(timestamp: datetime.timestamp, server: ServerUTC):
+    '''converts unix timestamp to local time'''
+    return timestamp - server.value*3600
+
+if __name__ == "__main__":
+    a = [1680821100, 1680780600, 1680781500, 1680784200, 1680776100, 1680821100, 1680781500,
+         1680785100, 1680787800]
+    
+    for k in vars(DailyEvents):
+        print(k)
