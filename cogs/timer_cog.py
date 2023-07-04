f7d1b7e1ee00e6292323cd5304ddd50e4cb220b6
ScobraCk <scobrack@gmail.com>
2023-07-04 12:40:47 +0900

    remove initial load function

--- cogs/timer_cog.py
+++ cogs/timer_cog.py
@@ -64,28 +64,28 @@ class TimerCog(commands.Cog, name = 'Timer Cog'):
     async def cog_unload(self):
         self.my_task.cancel()
 
-    async def cog_load(self):  # temporary measure to initiate
-        self.bot.block_guilddb = True
-        time.sleep(1)  # in case there is other use, need further error checking implemented
+    # async def cog_load(self):  # temporary measure to initiate
+    #     self.bot.block_guilddb = True
+    #     time.sleep(1)  # in case there is other use, need further error checking implemented
 
-        db = GuildDB()
-        worlds = fetch_worlddata()
-        for world_data in worlds:
-            server = int(str(world_data['world_id'])[0])
-            world = int(str(world_data['world_id'])[1:])
+    #     db = GuildDB()
+    #     worlds = fetch_worlddata()
+    #     for world_data in worlds:
+    #         server = int(str(world_data['world_id'])[0])
+    #         world = int(str(world_data['world_id'])[1:])
 
-            if world_data['ranking'] == False:
-                continue
+    #         if world_data['ranking'] == False:
+    #             continue
 
-            data = fetch_guildlist(server, world)
-            if data['status'] != 200:
-                continue
-            print(world_data['world_id'])
-            guilds = data['data']['rankings']['bp']
-            db.insert_guilds(guilds, server, world)
+    #         data = fetch_guildlist(server, world)
+    #         if data['status'] != 200:
+    #             continue
+    #         print(world_data['world_id'])
+    #         guilds = data['data']['rankings']['bp']
+    #         db.insert_guilds(guilds, server, world)
 
-        db.close()
-        self.bot.block_guilddb = False
+    #     db.close()
+    #     self.bot.block_guilddb = False
 
     @tasks.loop(time=cur_time)
     async def my_task(self):
@@ -114,3 +114,4 @@ class TimerCog(commands.Cog, name = 'Timer Cog'):
      
 async def setup(bot):
 	await bot.add_cog(TimerCog(bot))
+        
