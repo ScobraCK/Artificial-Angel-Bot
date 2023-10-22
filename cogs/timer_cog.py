c187fe1976fe89c75f27a4d342b4d8e2a07a0143
ScobraCk <scobrack@gmail.com>
2023-10-22 22:10:20 +0900

    fixed current time funtion location

--- cogs/timer_cog.py
+++ cogs/timer_cog.py
@@ -45,8 +45,8 @@ class TimerCog(commands.Cog, name = 'Timer Cog'):
     @tasks.loop(time=cur_time)
     async def my_task(self):
         ch = self.bot.get_channel(LOG_CHANNEL)
-        msg = get_cur_time() + '\n'
-        msg += update_rankings(self.bot.gdb) 
+        ret_msg = update_rankings(self.bot.gdb) 
+        msg = f'{get_cur_time()}\n{ret_msg}'
         embed = discord.Embed(description=msg)
 
         await ch.send(embed=embed)
