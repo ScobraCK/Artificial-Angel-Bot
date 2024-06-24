9027c81b96e12fa28d54e0553282352c4854fd7d
ScobraCk <scobrack@gmail.com>
2024-06-24 10:23:39 +0900

    changed ranking update schedule to fixed time every 4 hours

--- aabot/cogs/timer_cog.py
+++ aabot/cogs/timer_cog.py
@@ -1,14 +1,12 @@
-import datetime, discord
 from discord.ext import commands, tasks
+from zoneinfo import ZoneInfo
+from datetime import time
 
 from mementodb import async_update_guild_rankings, async_update_player_rankings, update_guild_rankings, update_player_rankings
 from timezones import get_cur_time
 from main import AABot
 
-# utc = datetime.timezone.utc
 
-# # If no tzinfo is given then UTC is assumed.
-# cur_time = datetime.time(hour=13, tzinfo=utc)
 
 class TimerCog(commands.Cog, name = 'Timer Cog'):
     def __init__(self, bot: AABot):
@@ -20,7 +18,14 @@ class TimerCog(commands.Cog, name = 'Timer Cog'):
     async def cog_unload(self):
         self.update_ranking.cancel()
 
-    @tasks.loop(hours=4)
+    @tasks.loop(
+        time=[time(hour=0, tzinfo=ZoneInfo("UTC")),
+              time(hour=4, tzinfo=ZoneInfo("UTC")),
+              time(hour=8, tzinfo=ZoneInfo("UTC")),
+              time(hour=12, tzinfo=ZoneInfo("UTC")),
+              time(hour=16, tzinfo=ZoneInfo("UTC")),
+              time(hour=20, tzinfo=ZoneInfo("UTC"))]
+        )
     async def update_ranking(self):
         ch = self.bot.get_channel(self.bot.log_channel)
         
