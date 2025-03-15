a6141e9c6be420dbc07395c3e7efafe5700a9178
ScobraCk <scobrack@gmail.com>
2025-03-15 20:57:05 +0900

    fix typo

--- api/crud/mentemori.py
+++ api/crud/mentemori.py
@@ -64,7 +64,7 @@ def update_guilds(session: Session, guild_data: dict):
         for guild_id, guild in guild_info.items():
             guild_id = int(guild_id)
             update_data = {
-                "guild_id": guild_id,
+                "id": guild_id,
                 "world_id": world_id,
                 "name": guild["name"],
                 "bp": guild["bp"],
