1561cb1d995574d9f354abd2ecd3374d9289bd9c
ScobraCk <scobrack@gmail.com>
2026-04-28 21:03:20 +0900

    increase timeout for mentemori

--- api/utils/mentemori.py
+++ api/utils/mentemori.py
@@ -11,7 +11,7 @@ PLAYER = '0/player_ranking/latest'
 GUILD = '0/guild_ranking/latest'
 
 async def fetch_mentemori(path: str):
-    async with httpx.AsyncClient(base_url=API_BASE, timeout=10) as client:
+    async with httpx.AsyncClient(base_url=API_BASE, timeout=30) as client:
         try:
             response = await client.get(path)
             response.raise_for_status()
